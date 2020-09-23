fda4d9c8856cf374b16be488ba411f170c6d2d96
edmundo papi <edmundopierre@yahoo.com>
2020-09-23 08:40:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,41 +16,42 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/articles/:id' do
-    id = params[:id]
-    @article = Article.find_by(id:id)
-    erb :show
-  end
-
   get '/articles/new' do
     erb :new
   end
 
   post '/articles' do
     attrs = params
-    @article=Article.create(attrs)
+    @article = Article.create(attrs)
 
-    redirect '/show'
-  end
-
-  get '/articles/:id/edit' do
-     id = params[:id]
-     @article = Article.find_by(id: id)
-     erb :'edit'
-  end
-
-  patch '/articles/:id' do
-    id = parms[:id]
-    @article =Aritcle.find_by(id: id)
-    attrs = params[:article]
-    @aricle.update(attrs)
     redirect to "/articles/#{@article.id}"
   end
 
-  delete '/article/:id' do
-   id = params[:id]
-   Article.destroy(id)
-   redirect to '/articles'
+  get '/articles/:id' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
 
+    erb :show
   end
+
+  get '/articles/:id/edit' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+    attrs = params[:article]
+    @article.update(attrs)
+    redirect to "/articles/#{@article.id}"
+  end
+
+    delete '/articles/:id' do
+    id = params[:id]
+    Article.destroy(id)
+    redirect to '/articles'
+    end
+
 end
